570f92d219a96ba6584acdddb409fc562b3461f0
kanishkasakthivel1206-png <kanishkasakthivel1206@gmail.com>
2026-09-18 19:57:31 +0530

    Added Calculator Program

---
+++ CodeAlpha_BasicCalculator.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+
+int main()
+{
+    float num1, num2, result;
+    char operator;
+
+    // Get input from the user
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    // Perform the selected operation
+    switch (operator)
+    {
+        case '+':
+            result = num1 + num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '/':
+            if (num2 == 0)
+            {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            else
+            {
+                result = num1 / num2;
+                printf("Result = %.2f\n", result);
+            }
+            break;
+
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
+```
